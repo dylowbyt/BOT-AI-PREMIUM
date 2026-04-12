ea6c47e7220313be8514c42e69770a12d078621c
dylowbyt <andipebrianto12@gmail.com>
2026-04-12 23:49:54 +0700

    Create package.js

---
+++ server/package.js
@@ -0,0 +1,10 @@
+{
+  "name": "xyabot-license-api",
+  "version": "1.0.0",
+  "main": "server.js",
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "dayjs": "^1.11.10"
+  }
+}
